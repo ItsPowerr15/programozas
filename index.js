244d174d3e9d5aed6e55feec7b4100953ae0e2fa
unknown <diak@fonix.kag.hu>
2022-10-04 14:00:43 +0200

    elso commit messagem

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log(`András
+Koppány
+Andris
+Bendegúz
+
+Gellén András
+2007.03.09
+180cm`);
+
+console.log(22 + 13)
+console.log(22 - 13)
+console.log(22 * 13)
+console.log(22 / 13)
+console.log(22 / 13)
+console.log(15 / 30 * 100)
+
+let varible = 23
+console.log('a kedvenc számom a: 23');
+
+let a = 123
+let b = 526
+let c;
+
+c = a;
+a = b;
+b = c;
+
+console.log('a erteke', a);
+console.log('b erteke', b);
+
+let varible1 = `${63} kg`;
+let varible2 = `${180} cm`;
+console.log(varible1);
+console.log(varible2);
+
+
+let d = 3;
+
+a = 12;
+
+if (d < 5) {
+    console.log('kisebb');
+} else if (d == 10) {
+    console.log('a egyenlő 10');
+} else {
+    console.log('hello');
+}
+
+if (d < 5 || d > 10) {
+    console.log('cica');
+}
+
+if (d < 5 && d == 3) {
+    console.log('kutya');
+}
+
+// mellette lévő dolgok azok kommentek
+
+// let varible = 'a';
+// let varible2 = 12;
+// let varible3 = false;
+
+// let varible4 = varible2 + 32;
+
+// varible = varible + 32;
+// varible2 += 32;
+
+// const constvarible = 12;
+
+// constvarible = 14;
+
+// console.log(varible2);
