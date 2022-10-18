c4b27488aadea8772c121698ebacd7d9dc6ab5a4
unknown <diak@fonix.kag.hu>
2022-10-18 14:15:00 +0200

    commit

--- index.js
+++ index.js
@@ -1,90 +1,115 @@
-console.log(`András
-Koppány
-Andris
-Bendegúz
+// console.log(`András
+// Koppány
+// Andris
+// Bendegúz
 
-Gellén András
-2007.03.09
-180cm`);
+// Gellén András
+// 2007.03.09
+// 180cm`);
 
-console.log(22 + 13)
-console.log(22 - 13)
-console.log(22 * 13)
-console.log(22 / 13)
-console.log(22 / 13)
-console.log(15 / 30 * 100)
+// console.log(22 + 13)
+// console.log(22 - 13)
+// console.log(22 * 13)
+// console.log(22 / 13)
+// console.log(22 / 13)
+// console.log(15 / 30 * 100)
 
-let varible = 23
-console.log('a kedvenc számom a: 23');
+// let varible = 23
+// console.log('a kedvenc számom a: 23');
 
-let a = 123
-let b = 526
-let c;
+// let a = 123
+// let b = 526
+// let c;
 
-c = a;
-a = b;
-b = c;
+// c = a;
+// a = b;
+// b = c;
 
-console.log('a erteke', a);
-console.log('b erteke', b);
+// console.log('a erteke', a);
+// console.log('b erteke', b);
 
-let varible1 = `${63} kg`;
-let varible2 = `${180} cm`;
-console.log(varible1);
-console.log(varible2);
-
-// mellette lévő dolgok azok kommentek
-
-// let varible = 'a';
-// let varible2 = 12;
-// let varible3 = false;
-
-// let varible4 = varible2 + 32;
-
-// varible = varible + 32;
-// varible2 += 32;
-
-// const constvarible = 12;
-
-// constvarible = 14;
-
+// let varible1 = `${63} kg`;
+// let varible2 = `${180} cm`;
+// console.log(varible1);
 // console.log(varible2);
 
+// // mellette lévő dolgok azok kommentek
 
-let d = 3;
+// // let varible = 'a';
+// // let varible2 = 12;
+// // let varible3 = false;
 
-a = 12;
+// // let varible4 = varible2 + 32;
 
-if (d < 5) {
-    console.log('kisebb');
-} else if (d == 10) {
-    console.log('a egyenlő 10');
-} else {
-    console.log('hello');
+// // varible = varible + 32;
+// // varible2 += 32;
+
+// // const constvarible = 12;
+
+// // constvarible = 14;
+
+// // console.log(varible2);
+
+
+// let d = 3;
+
+// a = 12;
+
+// if (d < 5) {
+//     console.log('kisebb');
+// } else if (d == 10) {
+//     console.log('a egyenlő 10');
+// } else {
+//     console.log('hello');
+// }
+
+// if (d < 5 || d > 10) {
+//     console.log('cica');
+// }
+
+// if (d < 5 && d == 3) {
+//     console.log('kutya');
+// }
+
+
+// let e = 1;
+// while (e <= 100) {
+//     console.log(e);
+//     e++;
+// }
+
+// for (let f = 1; f <= 100; f++) {
+//     console.log(f);
+// }
+
+// let g = 2;
+// while (g <= 500) {
+//     console.log(g);
+//     g += 2;
+// }
+
+
+function akarmi(a) {
+    console.log("Hello", a)
 }
 
-if (d < 5 || d > 10) {
-    console.log('cica');
+akarmi("andras");
+
+function valami(b) {
+    return b * 2;
 }
 
-if (d < 5 && d == 3) {
-    console.log('kutya');
+console.log(valami(21));
+
+function random(c, d) {
+    return c + d;
 }
 
+console.log(random(12, 18));
 
-let e = 1;
-while (e <= 100) {
-    console.log(e);
-    e++;
+function faktorial(e){
+    while(e < 100)
+    e++
+    console.log(e)
 }
-
-for (let f = 1; f <= 100; f++) {
-    console.log(f);
-}
-
-let g = 2;
-while (g <= 500) {
-    console.log(g);
-    g += 2;
-}
-
+faktorial(3);
